0b08207aee6106bff2bad492bd8788efc30f4366
B. van Berkum <dev@dotmpe.com>
2012-11-10 01:03:10 +0100

    Adding client.

---
+++ client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import urllib2
+
+def main(args):
+
+    cmd = 'list'
+    if args:
+        cmd = args.pop()
+    fl = urllib2.urlopen('http://dandy.local:8081/%s' % cmd)
+    print fl.read()
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:]);
